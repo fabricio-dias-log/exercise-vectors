a83a7a8b1083f829fcb849783eb86e082d07e13c
Fabricio Dias <fabricio270909hotmail.com>
2024-06-12 23:00:24 -0300

    feat: console menu

--- ExerciseVectors/Program.cs
+++ ExerciseVectors/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int option;
+            do
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Option list:");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("1 - Register a room");
+                Console.WriteLine("2 - Register a student");
+                Console.WriteLine("3 - Rent a room");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Choose an option: ");
+                option = int.Parse(Console.ReadLine());
+
+                doChoosedFunction(option);
+            } while (option != 0);
+        }
+        static void doChoosedFunction(int choosedOption) {
+            switch (choosedOption)
+            {
+                case 1:
+                    
+                    break;
+                default:
+                    return;
+            }
         }
     }
+    
+    
 }
