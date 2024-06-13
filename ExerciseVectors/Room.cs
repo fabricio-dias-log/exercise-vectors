134a22291a6afc6ab006ee3a4e46ff9d8a1ca027
Fabricio Dias <fabricio270909hotmail.com>
2024-06-12 23:05:29 -0300

    feat: attributes and initial register function

--- ExerciseVectors/Room.cs
+++ ExerciseVectors/Room.cs
@@ -9,5 +9,25 @@ namespace ExerciseVectors
     internal class Room
     {
         public int Number { get; set; }
+        public bool HasStudent { get; private set; }
+        public string? StudentName { get;  private set; }
+
+        public Room(int number, bool hasStudent)
+        {
+            Number = number;
+            HasStudent = false;
+        }
+        
+        public Room(int number, bool hasStudent,string? studentName) : this(number, hasStudent)
+        {
+            StudentName = studentName;
+        }
+
+        public Room registerRoom(int number)
+        {
+            return this;
+        }
+        
+        
     }
 }
